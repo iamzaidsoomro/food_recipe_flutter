d579fcea1bab8c503bb6995bba9d024553d56a8d
iamzaidsoomro <zaid3614@gmail.com>
2022-07-20 12:45:13 +0500

    Meal Type Page

--- food_recipe/lib/components/Categories.dart
+++ food_recipe/lib/components/Categories.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:food_recipe/components/MealList.dart';
 
 class Categories extends StatelessWidget {
   @override
@@ -15,29 +16,41 @@ class Categories extends StatelessWidget {
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
-            Card(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              child: Container(
-                width: 100,
-                height: 100,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.egg, color: Theme.of(context).primaryColor),
-                      const SizedBox(height: 10),
-                      Text("Breakfast",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).primaryColor,
-                          )),
-                    ]),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => MealList(mealType: "Breakfast Meals"),
+                  ),
+                );
+              },
+              child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.egg, color: Theme.of(context).primaryColor),
+                        const SizedBox(height: 10),
+                        Text("Breakfast",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).primaryColor,
+                            )),
+                      ]),
+                ),
               ),
             ),
             GestureDetector(
               onTap: () {
-                Navigator.pushNamed(context, "/meal_list");
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return MealList(mealType: "Lunch Meals");
+                }));
               },
               child: Card(
                 elevation: 5,
@@ -61,25 +74,32 @@ class Categories extends StatelessWidget {
                 ),
               ),
             ),
-            Card(
-              elevation: 5,
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10)),
-              child: Container(
-                width: 100,
-                height: 100,
-                child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Icon(Icons.restaurant,
-                          color: Theme.of(context).primaryColor),
-                      const SizedBox(height: 10),
-                      Text("Dinner",
-                          style: TextStyle(
-                            fontWeight: FontWeight.bold,
-                            color: Theme.of(context).primaryColor,
-                          )),
-                    ]),
+            GestureDetector(
+              onTap: () {
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return MealList(mealType: "Dinner Meals");
+                }));
+              },
+              child: Card(
+                elevation: 5,
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10)),
+                child: Container(
+                  width: 100,
+                  height: 100,
+                  child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(Icons.restaurant,
+                            color: Theme.of(context).primaryColor),
+                        const SizedBox(height: 10),
+                        Text("Dinner",
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: Theme.of(context).primaryColor,
+                            )),
+                      ]),
+                ),
               ),
             )
           ],
--- food_recipe/lib/components/MealList.dart
+++ food_recipe/lib/components/MealList.dart
@@ -1,8 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:food_recipe/components/RecipePage.dart';
+
+import '../apiCall.dart';
 
 class MealList extends StatelessWidget {
+  final mealType;
+  const MealList({Key? key, this.mealType}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Text("Test");
+    Future data = getDishes(mealType);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(mealType,
+            style: TextStyle(color: Theme.of(context).primaryColor)),
+        centerTitle: true,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+        iconTheme: IconThemeData(color: Theme.of(context).primaryColor),
+      ),
+      body: FutureBuilder(
+        future: data,
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          if (snapshot.data == null) {
+            return Container(
+              child: Center(
+                child: CircularProgressIndicator(),
+              ),
+            );
+          } else {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.height,
+              child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                itemCount: snapshot.data['hits'].length,
+                itemBuilder: (BuildContext context, int index) {
+                  return (ListTile(
+                    style: ListTileStyle.list,
+                    leading: Image.network(
+                      snapshot.data['hits'][index]['recipe']['image'],
+                      height: 100,
+                      width: 100,
+                      fit: BoxFit.cover,
+                    ),
+                    title: Text(
+                      snapshot.data['hits'][index]['recipe']['label'],
+                      style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Theme.of(context).primaryColor),
+                    ),
+                    subtitle: Text(
+                      snapshot.data['hits'][index]['recipe']['source'],
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    trailing: Text(
+                      snapshot.data['hits'][index]['recipe']['dishType'][0],
+                      style: TextStyle(
+                        fontSize: 13,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey,
+                      ),
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => RecipePage(
+                            name: snapshot.data['hits'][index]['recipe']
+                                ['label'],
+                            ingredients: snapshot.data['hits'][index]['recipe']
+                                ['ingredients'],
+                            image: snapshot.data['hits'][index]['recipe']
+                                ['image'],
+                            source: snapshot.data['hits'][index]['recipe']
+                                ['source'],
+                            cuisine: snapshot.data['hits'][index]['recipe']
+                                ['cuisineType'],
+                            nutrition: snapshot.data['hits'][index]['recipe']
+                                ['totalNutrients'],
+                            mealType: snapshot.data['hits'][index]['recipe']
+                                ['mealType'],
+                          ),
+                        ),
+                      );
+                    },
+                  ));
+                },
+              ),
+            );
+          }
+        },
+      ),
+    );
   }
 }
